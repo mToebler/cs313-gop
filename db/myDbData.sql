f694727e399b860045f1f28e6fbea8b60baa3fd4
Mark Tobler <marktobler@gmail.com>
2019-10-18 23:39:01 -0700

    Database work prep2

--- db/myDbData.sql
+++ db/myDbData.sql
@@ -1,30 +1,30 @@
 -- location data needed!
-INSERT INTO locations (name, description) VALUES ("A1", "northeast corner");
-INSERT INTO locations (name, description) VALUES ("A2", "along north wall eastern side");
-INSERT INTO locations (name, description) VALUES ("A3", "along north wall middle");
-INSERT INTO locations (name, description) VALUES ("A4", "along north wall western side");
-INSERT INTO locations (name, description) VALUES ("A5", "northwest corner");
-INSERT INTO locations (name, description) VALUES ("B1", "door way");
-INSERT INTO locations (name, description) VALUES ("B2", "in front of door");
-INSERT INTO locations (name, description) VALUES ("B3", "near north wall middle");
-INSERT INTO locations (name, description) VALUES ("B4", "near north wall western side");
-INSERT INTO locations (name, description) VALUES ("B5", "near northwest corner at wall");
-INSERT INTO locations (name, description) VALUES ("C1", "middle at eastern wall");
-INSERT INTO locations (name, description) VALUES ("C2", "middle near eastern wall");
-INSERT INTO locations (name, description) VALUES ("C3", "middle of garage");
-INSERT INTO locations (name, description) VALUES ("C4", "middle near western side");
-INSERT INTO locations (name, description) VALUES ("C5", "middle at western wall");
-INSERT INTO locations (name, description) VALUES ("D1", "near southeast corner at wall");
-INSERT INTO locations (name, description) VALUES ("D2", "near south wall eastern side");
-INSERT INTO locations (name, description) VALUES ("D3", "near south wall middle");
-INSERT INTO locations (name, description) VALUES ("D4", "near south wall western side");
-INSERT INTO locations (name, description) VALUES ("D5", "near southwest corner at wall");
-INSERT INTO locations (name, description) VALUES ("E1", "southeast corner");
-INSERT INTO locations (name, description) VALUES ("E2", "along south wall eastern side");
-INSERT INTO locations (name, description) VALUES ("E3", "along south wall middle");
-INSERT INTO locations (name, description) VALUES ("E4", "along south wall western side");
-INSERT INTO locations (name, description) VALUES ("E5", "southwest corner");
+INSERT INTO locations (name, description) VALUES ('A1', 'northeast corner');
+INSERT INTO locations (name, description) VALUES ('A2', 'along north wall eastern side');
+INSERT INTO locations (name, description) VALUES ('A3', 'along north wall middle');
+INSERT INTO locations (name, description) VALUES ('A4', 'along north wall western side');
+INSERT INTO locations (name, description) VALUES ('A5', 'northwest corner');
+INSERT INTO locations (name, description) VALUES ('B1', 'door way');
+INSERT INTO locations (name, description) VALUES ('B2', 'in front of door');
+INSERT INTO locations (name, description) VALUES ('B3', 'near north wall middle');
+INSERT INTO locations (name, description) VALUES ('B4', 'near north wall western side');
+INSERT INTO locations (name, description) VALUES ('B5', 'near northwest corner at wall');
+INSERT INTO locations (name, description) VALUES ('C1', 'middle at eastern wall');
+INSERT INTO locations (name, description) VALUES ('C2', 'middle near eastern wall');
+INSERT INTO locations (name, description) VALUES ('C3', 'middle of garage');
+INSERT INTO locations (name, description) VALUES ('C4', 'middle near western side');
+INSERT INTO locations (name, description) VALUES ('C5', 'middle at western wall');
+INSERT INTO locations (name, description) VALUES ('D1', 'near southeast corner at wall');
+INSERT INTO locations (name, description) VALUES ('D2', 'near south wall eastern side');
+INSERT INTO locations (name, description) VALUES ('D3', 'near south wall middle');
+INSERT INTO locations (name, description) VALUES ('D4', 'near south wall western side');
+INSERT INTO locations (name, description) VALUES ('D5', 'near southwest corner at wall');
+INSERT INTO locations (name, description) VALUES ('E1', 'southeast corner');
+INSERT INTO locations (name, description) VALUES ('E2', 'along south wall eastern side');
+INSERT INTO locations (name, description) VALUES ('E3', 'along south wall middle');
+INSERT INTO locations (name, description) VALUES ('E4', 'along south wall western side');
+INSERT INTO locations (name, description) VALUES ('E5', 'southwest corner');
 
 
 -- items
-INSERT INTO items (name, value) VALUES    '""
+--INSERT INTO items (name, value) VALUES
